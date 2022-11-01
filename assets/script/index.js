15f4028bd3a1f199fa3f8ca80083b436be69b167
Paulo Marques <paulomarques.rs1@gmail.com>
2022-11-01 07:23:18 -0300

    Add - remoção automatica de jogos passados

---
+++ assets/script/index.js
@@ -0,0 +1,18 @@
+const cardJogos = document.querySelectorAll(".valid")
+
+for(var i=0; i<cardJogos.length; i++){
+    const data = cardJogos[i].querySelector(".card__info-data").textContent.slice(0, 5).split('/').reverse().join('/');
+
+    const today = new Date()
+    const day = today.getDate()
+    const month = today.getMonth()+1
+    const current = `${month}/${day}`
+
+    if (data<current){
+        console.log(data.slice(0, 5))
+        console.log(today)
+        cardJogos[i].classList.add("hidden")
+    }
+    
+}
+
